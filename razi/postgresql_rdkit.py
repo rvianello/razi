10c364307cce444e55dfaf6d0e6b49c451b15568
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-06-01 21:47:57 +0200

    pg+rdkit ddl

--- razi/postgresql_rdkit.py
+++ razi/postgresql_rdkit.py
@@ -2,7 +2,9 @@
 from sqlalchemy import select, func, and_
 
 from razi.chem import ChemComparator
-from razi.expression import PersistentMoleculeElement, TxtMoleculeElement
+from razi.chemtypes import Molecule
+from razi.expression import PersistentMoleculeElement, \
+    TxtMoleculeElement, TxtPatternElement
 from razi.dialect import ChemicalDialect 
 from razi.functions import functions, BaseFunction
 
@@ -42,29 +44,49 @@ class PostgresRDKitDialect(ChemicalDialect):
     """Implementation of ChemicalDialect for PostgreSQL+RDKit."""
     
     __functions = {
-                   #functions.func1: 'Func1',
-                   #pgrdkit_functions.func2 : 'Func2',
-                  }
+        #TxtMoleculeElement: '',
+        #TxtPatternElement: '',
+        
+        #functions.smiles: '',
+        #functions.mw: '',
+        #functions.logp: '',
+        #functions.tpsa: '',
+        #functions.hba: '',
+        #functions.hbd: '',
+        #functions.num_atoms: '',
+        #functions.num_hetatoms: '',
+        #functions.num_hvy_atoms: '',
+        #functions.num_rings: '',
+        #functions.num_rotatable_bonds: '',
+        
+        #functions.equals: pgrdkit_functions._equals,
+        #functions.contains: pgrdkit_functions._contains,
+        #functions.contained_in: pgrdkit_functions._contained_in, 
+        #functions.matches: pgrdkit_functions._matches,                    #functions.func1: 'Func1',
+        
+        #pgrdkit_functions.func2 : 'Func2',
+        }
     
     def _get_function_mapping(self):
         return PostgresRDKitDialect.__functions
     
     def process_result(self, value, type):
-        return PostgresRDKitPersistentMoleculeElement(TxtMoleculeElement(value))
+        return PostgresRDKitPersistentMoleculeElement(value)
     
-    def handle_ddl_before_drop(self, bind, table, column):
-        # drop the chemical column
+    def db_column_type(self, type_):
+        if isinstance(type_, Molecule):
+            return 'mol'
+        raise NotImplementedError("DB column for type %s not supported by the "
+                                  "current chemical dialect " % type(type_))
+
+    def _ddl_before_drop(self, table, column, bind):
+        # nothing to do?
         pass
     
-    def handle_ddl_after_create(self, bind, table, column):    
-        # add the chemical column
-
-        # eventually also add a structural index and not NULL constraint 
+    def _ddl_after_create(self, table, column, bind):    
+        # eventually add a structural index and not NULL constraint 
         if column.type.chemical_index:
             bind.execute("CREATE INDEX \"idx_%s_%s\" ON \"%s\".\"%s\" USING GIST (%s)" % 
-                         (table.name, column.name, (table.schema or 'public'), table.name, column.name))
-            
-        if not column.nullable:
-            bind.execute("ALTER TABLE \"%s\".\"%s\" ALTER COLUMN \"%s\" SET not null" % 
-                            ((table.schema or 'public'), table.name, column.name))
+                         (table.name, column.name, (table.schema or 'public'), 
+                          table.name, column.name))
             
