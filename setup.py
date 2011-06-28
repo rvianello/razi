89fd3dfbb0464798bd8be1587d3efd43bc9aad8f
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-06-28 22:12:41 +0200

    dependencies and additional metadata

--- setup.py
+++ setup.py
@@ -4,10 +4,24 @@ from setuptools import setup, find_packages
 
 setup(name='razi',
       version='0.0.0',
-      description='',
-      #url='',
+      description='Using SQLAlchemy with chemical databases',
+      classifiers=[
+          "Development Status :: 1 - Planning",
+          "Environment :: Plugins",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Scientific/Engineering :: Chemistry"
+      ],
+      url='http://razi.readthedocs.org/',
+      keywords='chemistry cheminformatics sqlalchemy orm',
       packages = find_packages(exclude = [
             'docs',
             'docs.*',
             ]),
+      install_requires=[
+          'SQLAlchemy>=0.7.0',
+      ],
      )
