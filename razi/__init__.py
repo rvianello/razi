49d52b5a28e2491b5033052b37988ff22561fab0
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-07-25 22:44:57 +0200

    cleanup

--- razi/__init__.py
+++ razi/__init__.py
@@ -1,11 +1,1 @@
-import inspect
-
-import razi.dialect
-
-    
-__all__ = sorted(name for name, obj in locals().items()
-                 if not (name.startswith('_') or inspect.ismodule(obj)))
-
 __version__ = '0.0.0'
-
-del inspect
