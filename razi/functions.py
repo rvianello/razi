d54b2a668c7e4929ab7f5642f644e95066ddca51
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-07-30 22:48:34 +0200

    mol constructors and converters

--- razi/functions.py
+++ razi/functions.py
@@ -1,10 +1,50 @@
+from sqlalchemy import String
 from sqlalchemy.sql import functions
 from sqlalchemy.dialects import postgresql
 
 from . import types
 
-class mol_from_pkl(functions.GenericFunction):
-    type = types.Mol
+# GenericFunction subclasses strongly based on the geoalchemy2
+# implementation
 
-class mol_to_pkl(functions.GenericFunction):
-    type = postgresql.BYTEA
+_FUNCTIONS = [
+    #
+    # mol constructors
+    #
+
+    ('mol_from_smiles', types.Mol, None,),
+    ('mol_from_smarts', types.Mol, None,),
+    ('mol_from_ctab', types.Mol, None,),
+    ('mol_from_pkl', types.Mol, None,),
+
+    ('mol_adjust_query_properties', types.Mol, None,),
+
+    #
+    # mol conversion functions
+    #
+
+    ('mol_to_smiles', String, None,),
+    ('mol_to_smarts', String, None,),
+    ('mol_to_ctab', String, None,),
+    ('mol_to_svg', String, None,),
+    ('mol_to_pkl', postgresql.BYTEA, None,),
+]
+
+# Iterate through _FUNCTION and create GenericFunction classes dynamically
+for name, type_, doc in _FUNCTIONS:
+    attributes = {'name': name}
+    docs = []
+
+    if doc is not None:
+        docs.append(doc)
+
+    if type_ is not None:
+        attributes['type'] = type_
+
+        type_str = '{0}.{1}'.format(type_.__module__, type_.__name__)
+        docs.append('Return type: :class:`{0}`.'.format(type_str))
+
+    if len(docs) != 0:
+        attributes['__doc__'] = '\n\n'.join(docs)
+
+    globals()[name] = type(name, (functions.GenericFunction,), attributes)
