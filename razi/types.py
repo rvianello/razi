606727441585dac5c4e0dc5654cb7b986a6fd949
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-08-01 17:12:15 +0200

    small fixes

--- razi/types.py
+++ razi/types.py
@@ -2,6 +2,8 @@ from sqlalchemy import func
 from sqlalchemy.types import UserDefinedType, TypeEngine
 
 from rdkit.Chem import AllChem as Chem
+from rdkit import DataStructs
+from rdkit.DataStructs import ExplicitBitVect
 
 from . import comparator
 
@@ -12,12 +14,6 @@ class Mol(UserDefinedType):
     def get_col_spec(self, **kw):
         return 'mol'
 
-    def bind_expression(self, bindvalue):
-        return func.mol_from_pkl(bindvalue)
-
-    def column_expression(self, col):
-        return func.mol_to_pkl(col, type_=self)
-
     def bind_processor(self, dialect):
         def process(value):
             # convert the Molecule instance to the value used by the
@@ -26,6 +22,12 @@ class Mol(UserDefinedType):
                 value = memoryview(value.ToBinary())
             return value
         return process
+
+    def bind_expression(self, bindvalue):
+        return func.mol_from_pkl(bindvalue)
+
+    def column_expression(self, col):
+        return func.mol_to_pkl(col, type_=self)
 
     def result_processor(self, dialect, coltype):
         def process(value):
@@ -54,20 +56,18 @@ class Bfp(UserDefinedType):
     def get_col_spec(self, **kw):
         return 'bfp'
 
+    def bind_processor(self, dialect):
+        def process(value):
+            if isinstance(value, ExplicitBitVect):
+                value = memoryview(DataStructs.BitVectToBinaryText(value))
+            return value
+        return process
+
     def bind_expression(self, bindvalue):
         return func.bfp_from_binary_text(bindvalue)
 
     def column_expression(self, col):
         return func.bfp_to_binary_text(col, type_=self)
-
-    def bind_processor(self, dialect):
-        def process(value):
-            # convert the Molecule instance to the value used by the
-            # db driver
-            if isinstance(value, Chem.Mol):
-                value = memoryview(DataStructs.BitVectToBinaryText(value))
-            return value
-        return process
 
     def result_processor(self, dialect, coltype):
         def process(value):
@@ -77,7 +77,7 @@ class Bfp(UserDefinedType):
                 return DataStructs.CreateFromBinaryText(bytes(value))
             else:
                 raise RuntimeError(
-                    "Unexpected row value type for a Mol instance")
+                    "Unexpected row value type for a Bfp instance")
         return process
 
 
