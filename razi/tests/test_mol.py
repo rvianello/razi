a51f07b8240a4305f69111381227aeff3c2429e9
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-08-04 17:39:00 +0200

    add stub tests module

---
+++ razi/tests/test_mol.py
@@ -0,0 +1,27 @@
+import unittest
+
+from sqlalchemy import create_engine, MetaData
+
+from razi.rdkit_postgresql.types import Mol
+
+
+engine = create_engine('postgresql://localhost/razi-rdkit-postgresql-test')
+metadata = MetaData(engine)
+
+molecules = Table(
+    'molecules', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('m', Mol), Index('molecules_m', 'm', postgresql_using='gist'),
+    )
+
+
+class MolTestCase(unittest.TestCase):
+
+    def setUp(self):
+        metadata.create_all()
+
+    def tearDown(self):
+        metadata.drop_all()
+
+    def test_this(self):
+        pass
