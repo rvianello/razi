270c2e6258a13aea15fb0641e800063563426f44
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-08-08 23:09:48 +0200

    add some tests of mol ops

--- razi/rdkit_postgresql/tests/test_mol.py
+++ razi/rdkit_postgresql/tests/test_mol.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import json
 
 from sqlalchemy import select, func
 from sqlalchemy import create_engine, MetaData
@@ -32,6 +34,33 @@ class MolBasicTestCase(unittest.TestCase):
 
         rs = engine.execute(select([ func.mol_from_smiles('') ]))
         self.assertIsInstance(rs.fetchall()[0][0], Chem.Mol)
+
+    def test_mol_from_ctab(self):
+
+        rs = engine.execute(select([ func.is_valid_ctab('xyz') ]))
+        self.assertFalse(rs.fetchall()[0][0])
+
+        ctab = '''chiral1.mol
+  ChemDraw04200416412D
+
+  5  4  0  0  0  0  0  0  0  0999 V2000
+   -0.0141    0.0553    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.8109    0.0553    0.0000 F   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.4266    0.7697    0.0000 Br  0  0  0  0  0  0  0  0  0  0  0  0
+   -0.0141   -0.7697    0.0000 Cl  0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8109   -0.1583    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  1  0
+  1  3  1  0
+  1  4  1  1
+  1  5  1  0
+M  END'''
+
+        rs = engine.execute(select([ func.is_valid_ctab(ctab) ]))
+        self.assertTrue(rs.fetchall()[0][0])
+
+        rs = engine.execute(select([ func.mol_from_ctab(ctab) ]))
+        self.assertIsInstance(rs.fetchall()[0][0], Chem.Mol)
+
 
     def test_mol_from_smarts(self):
 
@@ -68,6 +97,18 @@ class MolBasicTestCase(unittest.TestCase):
             rs.fetchall()[0][0],
             '[#6]1:[#6]:[#6]:[#6]:[#6]:[#6]:1')
 
+    def test_mol_samestruct(self):
+
+        rs = engine.execute(
+            select([ func.mol('Cc1ccccc1').samestruct(func.mol('c1ccccc1C')) ])
+        )
+        self.assertTrue(rs.fetchall()[0][0])
+
+        rs = engine.execute(
+            select([ func.mol('Cc1ccccc1').samestruct(func.mol('c1cnccc1C')) ])
+        )
+        self.assertFalse(rs.fetchall()[0][0])
+
 
 metadata = MetaData(engine)
 
@@ -77,7 +118,16 @@ molecules = Table(
     Column('m', Mol), Index('molecules_m', 'm', postgresql_using='gist'),
     )
 
-class MolTestCase(unittest.TestCase):
+
+_test_dir = os.path.dirname(__file__)
+for _ in range(3):
+    _test_dir = os.path.dirname(_test_dir)
+_test_dir = os.path.join(_test_dir, 'test_data')
+
+data_filepath = os.path.join(_test_dir, 'chembl_sample.json')
+
+
+class MolInsertTestCase(unittest.TestCase):
 
     def setUp(self):
         metadata.create_all()
@@ -85,5 +135,64 @@ class MolTestCase(unittest.TestCase):
     def tearDown(self):
         metadata.drop_all()
 
-    def test_this(self):
-        pass
+    def test_mol_insert(self):
+
+        self.assertTrue(os.path.exists(data_filepath))
+
+        with open(data_filepath, 'rt') as f:
+            sample_data = json.load(f)
+
+        engine.execute(
+            molecules.insert(),
+            [ { 'm': Chem.MolFromSmiles(s)} for _, s in sample_data ],
+            )
+
+        rs = engine.execute(select([func.count()]).select_from(molecules))
+
+        self.assertEqual(len(sample_data), rs.fetchall()[0][0])
+
+
+class MolTestCase(unittest.TestCase):
+
+    def setUp(self):
+        metadata.create_all()
+
+        with open(data_filepath, 'rt') as f:
+            sample_data = json.load(f)
+
+        engine.execute(
+            molecules.insert(),
+            [ { 'm': Chem.MolFromSmiles(s)} for _, s in sample_data ],
+            )
+
+    def tearDown(self):
+        metadata.drop_all()
+
+    def test_mol_ops(self):
+
+        molecules_count = select([func.count()]).select_from(molecules)
+
+        rs = engine.execute(molecules_count.where(
+            molecules.c.m.hassubstruct(Chem.MolFromSmiles('c1ccccc1'))
+        ))
+        self.assertEqual(830, rs.fetchall()[0][0])
+
+        rs = engine.execute(molecules_count.where(
+            molecules.c.m.hassubstruct(Chem.MolFromSmiles('c1ccncc1'))
+        ))
+        self.assertEqual(196, rs.fetchall()[0][0])
+
+        rs = engine.execute(molecules_count.where(
+            molecules.c.m.hassubstruct(func.mol_from_smarts('c1ccccc1'))
+        ))
+        self.assertEqual(830, rs.fetchall()[0][0])
+
+        rs = engine.execute(molecules_count.where(
+            molecules.c.m.hassubstruct(func.mol_from_smarts('c1ccncc1'))
+        ))
+        self.assertEqual(197, rs.fetchall()[0][0])
+
+        rs = engine.execute(molecules_count.where(
+            molecules.c.m.hassubstruct(func.mol_from_smarts('c1cc[c,n]cc1'))
+        ))
+        self.assertEqual(868, rs.fetchall()[0][0])
