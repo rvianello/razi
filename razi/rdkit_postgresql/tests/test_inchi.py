ca9d91457325db4ca0c67e64f783271967ecd408
Riccardo Vianello <riccardo.vianello@gmail.com>
2017-08-18 23:20:16 +0200

    test inchi output

---
+++ razi/rdkit_postgresql/tests/test_inchi.py
@@ -0,0 +1,27 @@
+import unittest
+
+from sqlalchemy import select, func
+from sqlalchemy import create_engine
+
+from rdkit.Chem import AllChem as Chem
+
+from razi.rdkit_postgresql.types import Mol
+
+
+engine = create_engine('postgresql://localhost/razi-rdkit-postgresql-test')
+
+class InChITestCase(unittest.TestCase):
+
+    def test_inchi_out(self):
+
+        rs = engine.execute(select([ func.mol_inchi(func.mol('c1ccccc1')) ]))
+        self.assertEqual(rs.fetchall()[0][0], 'InChI=1S/C6H6/c1-2-4-6-5-3-1/h1-6H')
+
+        rs = engine.execute(select([ func.mol_inchikey(func.mol('c1ccccc1')) ]))
+        self.assertEqual(rs.fetchall()[0][0], 'UHOVQNZJYSORNB-UHFFFAOYSA-N')
+
+        rs = engine.execute(select([ func.mol_inchi(func.mol('Cc1cc(C)[n+]c(C)c1')) ]))
+        self.assertEqual(rs.fetchall()[0][0], 'InChI=1S/C8H11N/c1-6-4-7(2)9-8(3)5-6/h4-5H,1-3H3/q+1')
+
+        rs = engine.execute(select([ func.mol_inchikey(func.mol('Cc1cc(C)[n+]c(C)c1')) ]))
+        self.assertEqual(rs.fetchall()[0][0], 'WDQXTFQYHUZLPX-UHFFFAOYSA-N')
